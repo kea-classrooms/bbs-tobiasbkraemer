5bdbe747cc6da2eae00545f1213b45d981282c0e
tobiasbkraemer <113188005+tobiasbkraemer@users.noreply.github.com>
2023-02-06 12:13:55 +0000

    NU ER DEN DER

--- fix.samples.sh
+++
@@ -1,8 +0,0 @@
- cd samples/
- mv animals/countries/ .
- mv countries/movies/ .
- mv animals/Monkies.txt animals/Monkeys.txt 
- echo Monkeys > animals/Monkeys.txt 
- mv movies/Germany.txt countries/
- mv countries/.apes.txt animals/
- cd -
