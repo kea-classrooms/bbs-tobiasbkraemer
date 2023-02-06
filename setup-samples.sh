0e875bbace660bcb67b65bfd201c4a2aaa633234
tobiasbkraemer <113188005+tobiasbkraemer@users.noreply.github.com>
2023-02-06 12:40:24 +0000

    Final

--- setup-samples.sh
+++ setup-samples.sh
@@ -1,26 +1,23 @@
-mkdir samples
-mkdir samples/animals
-mkdir samples/animals/countries
-mkdir samples/animals/countries/movies
+mkdir -p samples/animals/countries/movies
 
 ANIMALS=('Apes' 'Elephants' 'Elephant babies' 'Monkies' 'apes')
 COUNTRIES=('Denmark' 'Sweden' 'USA' 'France' 'Italy' '.apes' 'North Korea')
 MOVIES=('Batman' 'Sex And The City' 'JFK' 'Poltergeist' 'Germany')
 
 cd samples/animals 
-for id in ${ANIMALS[@]}
+for id in "${ANIMALS[@]}"
 do
-echo $id >> $id.txt
+echo "$id" >> "$id.txt"
 done
 
 cd countries
-for id in ${COUNTRIES[@]}
+for id in "${COUNTRIES[@]}"
 do
-echo $id >> $id.txt
+echo "$id" >> "$id.txt"
 done
 
 cd movies
-for id in ${MOVIES[@]}
+for id in "${MOVIES[@]}"
 do
-echo $id >> $id.txt
+echo "$id" >> "$id.txt"
 done
